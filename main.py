0cca308da64a5e105cd6e01c37e6f588da17a864
Elizabeth Clements <>
2023-02-01 00:26:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import discord
+import os
+from keep_alive import keep_alive
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+  t = message.created_at
+  hour = t.hour
+  channel = message.channel
+  print(message.author.id == 263178859971608586)
+  if message.author.id == 263178859971608586 and len(
+      message.attachments) > 0 and (hour >= 15 and
+                                    hour <= 23) and (channel.name == "memes"):
+    await message.channel.send("<@263178859971608586> get a job")
+
+
+keep_alive()
+my_secret = os.environ['DISCORD_BOT_SECRET']
+client.run(os.environ['DISCORD_BOT_SECRET'])
